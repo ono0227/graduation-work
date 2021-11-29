f7cd40fbd3f959fda594132fc2a672a2290d966e
小野 <pow@ononoMacBook-Air.local>
2021-11-29 17:50:00 +0900

    <head>タグとheader,footerの共通化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title = "")
+        base_title = "BIGBAG Store".freeze
+        if page_title.empty?
+          base_title
+        else
+          "#{page_title} - #{base_title}"
+        end
+      end
 end
