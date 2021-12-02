f3669f7742e840ca7215dff678467ad0291e0b07
小野 <pow@ononoMacBook-Air.local>
2021-12-02 19:03:00 +0900

    if文から三項演算子での処理に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def full_title(full_title = '')
+  def full_title(full_title)
     base_title = "BIGBAG Store".freeze
     full_title.blank? ? base_title : "#{full_title} - #{base_title}"
   end
