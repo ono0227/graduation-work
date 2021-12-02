bfc59415fb51fac7a7b7fa4f5f08266a359bff7d
小野 <pow@ononoMacBook-Air.local>
2021-12-02 13:37:10 +0900

    三項演算子を用いた処理に変更しました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
     base_title = "BIGBAG Store".freeze
-    if page_title.blank?
-      base_title
-    else
-      "#{page_title} - #{base_title}"
-    end
+    page_title.blank? ? 'base_title' : '#{page_title} - #{base_title}'
   end
 end
