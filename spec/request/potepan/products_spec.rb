a4d95cbf1367248611ccd68ae48d131408174d25
小野 <pow@ononoMacBook-Air.local>
2021-12-02 11:32:07 +0900

    テストの実装

--- spec/request/potepan/products_spec.rb
+++ spec/request/potepan/products_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   let(:product) { create(:product) }
-  
   before do
     get potepan_product_path(product.id)
   end
