7293ed11444b655e3feb2e1f046092aaffc22c30
小野 <pow@ononoMacBook-Air.local>
2021-12-03 18:00:01 +0900

    テスト対象のコンテンツを追加

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "Potepan::Products", type: :feature do
-  describe "display #show" do
-    let(:product) { create(:product) }
-    let(:image) { create(:image) }
+  let(:product) { create(:product) }
+  let(:image) { create(:image) }
 
-    background do
-      product.images << image
-      visit potepan_product_path(product.id)
-    end
+  background do
+    product.images << image
+    visit potepan_product_path(product.id)
+  end
 
-    scenario "商品画像が表示されること" do
-      expect(page).to have_selector('img, [src$="#{image.filename}"]')
-    end
+  scenario "商品情報が表示されること" do
+    expect(page).to have_content product.name
+    expect(page).to have_content product.display_price
+    expect(page).to have_content product.description
+    expect(page).to have_selector('img, [src$="#{image.filename}"]')
   end
 end
