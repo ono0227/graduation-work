37b07d7746108e025ed620b6221863cd0cb5616c
小野 <pow@ononoMacBook-Air.local>
2021-12-05 22:21:14 +0900

    レスポンステストに商品画像を追加しました

--- spec/request/potepan/products_spec.rb
+++ spec/request/potepan/products_spec.rb
@@ -12,10 +12,13 @@ RSpec.describe "Potepan::Products", type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it "productの名前/説明文/値段を取得、表示できること" do
-      expect(response.body).to include product.name
-      expect(response.body).to include product.description
-      expect(response.body).to include product.display_price.to_s
+    it "productの商品情報を取得、表示できること" do
+      within(".body") do
+        expect(response.body).to include product.name
+        expect(response.body).to include product.description
+        expect(response.body).to include product.display_price.to_s
+        expect(response.body).to include product.images
+      end
     end
   end
 end
