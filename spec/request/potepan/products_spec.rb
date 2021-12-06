5aae7f3d91cd4d9242effd53113194d73abf0512
小野 <pow@ononoMacBook-Air.local>
2021-12-06 13:36:31 +0900

    リクエストテストの対象項目を修正

--- spec/request/potepan/products_spec.rb
+++ spec/request/potepan/products_spec.rb
@@ -3,22 +3,10 @@ require 'rails_helper'
 RSpec.describe "Potepan::Products", type: :request do
   let(:product) { create(:product) }
 
-  before do
-    get potepan_product_path(product.id)
-  end
-
   describe "Get /potepan/products/:id" do
-    it 'リクエストが成功すること' do
-      expect(response).to have_http_status(:success)
-    end
-
-    it "productの商品情報を取得、表示できること" do
-      within(".body") do
-        expect(response.body).to include product.name
-        expect(response.body).to include product.description
-        expect(response.body).to include product.display_price.to_s
-        expect(response.body).to include product.images
-      end
+    it '商品詳細画面の表示に成功すること' do
+      get potepan_product_path(product.id)
+      expect(response).to have_http_status(200)
     end
   end
 end
