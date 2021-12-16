aaa22d70de6cd3913f75843b0a9eaf876f8cd9ec
ono0227 <pow@ononoMacBook-Air.local>
2021-12-16 17:55:28 +0900

    インデントを修正。余白を削除

--- spec/request/potepan/products_spec.rb
+++ spec/request/potepan/products_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe "Potepan::Products", type: :request do
   let(:taxon) { create(:taxon, taxonomy: taxonomy) }
   let(:taxonomy) { create(:taxonomy) }
   let(:image) { create(:image) }
-  let(:related_products) { create_list(:product, 4,taxons: [taxon]) }
+  let(:related_products) { create_list(:product, 4, taxons: [taxon]) }
 
   describe "Get /potepan/products/:id" do
     before do
       get potepan_product_path(product.id)
     end
 
-    it '商品詳細画面の表示に成功すること' do  
+    it '商品詳細画面の表示に成功すること' do
       expect(response).to have_http_status(200)
     end
 
     it '関連商品を取得し表示できること' do
-      within ('.productBox') do
+      within('.productBox') do
         related_products[0..3].all? do |related_product|
           expect(response.body).to include related_product.name
           expect(response.body).to include related_product.display_price.to_s
