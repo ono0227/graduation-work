bb405c720b7cfe7d93ba2d59a443b125a9d348ae
ono0227 <pow@ononoMacBook-Air.local>
2021-12-16 17:53:21 +0900

    改行を追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = Spree::Product.in_taxons(@product.taxons).includes(master: [:images, :default_price]).distinct.where.not(id: @product.id).limit(4)
+    @related_products = Spree::Product.in_taxons(@product.taxons).includes(master: [:images, :default_price]).
+      distinct.where.not(id: @product.id).limit(4)
   end
 end
