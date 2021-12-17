0686c4449d387f4822a5b60cf8861aa0b9a7b22e
ono0227 <pow@ononoMacBook-Air.local>
2021-12-17 23:32:47 +0900

    テストを追加で実装

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Potepan::Models", type: :model do
   let(:taxon) { create(:taxon, taxonomy[:taxonomy]) }
   let(:product) { create(:product, taxons: [taxon]) }
   let(:related_products) { create_list(:product, 4, taxons: [taxon]) }
+  let(:unrelated_products) { create_list(:product, 4) }
 
   it "関連商品を取得できること" do
     expect(product.related_products).to eq related_products
@@ -12,5 +13,13 @@ RSpec.describe "Potepan::Models", type: :model do
 
   it "関連商品を重複せず、取得できること" do
     expect(product.related_products).to eq related_products.uniq
+  end
+
+  it "関連しない商品は取得されないこと" do
+    expect(product.related_products).not_to eq unrelated_products
+  end
+
+  it "メインの商品が関連商品として取得されないこと" do
+    expect(product.related_products).not_to eq product
   end
 end
